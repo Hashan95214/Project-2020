95d4351971db987a6aa1c10f761184c81b164f39
Hashan95214 <gunarathna.pwhm@gmail.com>
2021-01-16 22:00:17 +0530

    update startup

--- cakeworld/Startup.cs
+++ cakeworld/Startup.cs
@@ -34,6 +34,13 @@ namespace cakeworld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(Options =>
+            Options.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
